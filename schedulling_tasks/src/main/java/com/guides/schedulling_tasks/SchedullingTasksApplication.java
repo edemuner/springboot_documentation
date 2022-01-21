f43dbaa68c44c754e51977008d81d26768e0670f
edemuner <eduardood15@hotmail.com>
2022-01-21 14:56:56 -0300

    inicializado o projeto schedulling_tasks e adicionada a dependência awaiility

---
+++ schedulling_tasks/src/main/java/com/guides/schedulling_tasks/SchedullingTasksApplication.java
@@ -0,0 +1,13 @@
+package com.guides.schedulling_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedullingTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedullingTasksApplication.class, args);
+	}
+
+}
---
+++ schedulling_tasks/src/test/java/com/guides/schedulling_tasks/SchedullingTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guides.schedulling_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedullingTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
